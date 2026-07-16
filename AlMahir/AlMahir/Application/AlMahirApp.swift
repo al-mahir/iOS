e99032f39ab161f969b8727b2736c7939bb5af79
Esraa Ehab <esraaehab530@gmail.com>
2026-07-16 09:07:39 -0700

    finished project structure and add gitignore file

---
+++ AlMahir/AlMahir/Application/AlMahirApp.swift
@@ -0,0 +1,18 @@
+//
+//  AlMahirApp.swift
+//  AlMahir
+//
+//  Created by Esraa Ehab on 16/07/2026.
+//
+
+import SwiftUI
+import Mushaf
+
+@main
+struct AlMahirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIView()
+        }
+    }
+}
---
+++ Modules/Mushaf/Sources/Mushaf/Presentation/View/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Esraa Ehab on 16/07/2026.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    public init() {}
+    
+    public var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
