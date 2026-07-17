d92263a3c6bbc90a56d706fa10b13afeef655768
alaaayman77 <alaaaymanfouad@gmail.com>
2026-07-17 06:53:36 -0700

    fixed duplicate font reference

--- Modules/Mushaf/Sources/Mushaf/Domain/Entities/MushafLine.swift
+++ Modules/Mushaf/Sources/Mushaf/Domain/Entities/MushafLine.swift
@@ -8,16 +8,3 @@
 import Foundation
 
 
-enum MushafLineType: String {
-    case ayah
-    case surahName = "surah_name"
-    case basmallah
-}
-
-struct MushafLine: Identifiable, Hashable {
-    let id: Int
-    let lineType: MushafLineType
-    let isCentered: Bool
-    let surahNumber: Int?
-    let words: [QuranWord]
-}
