8dafde5f052c06258764e82aa6c5b0f97e31e840
Basmala239 <abuziedbasmala@gmail.com>
2026-07-17 02:15:25 -0700

    set up general local database

---
+++ AlMahir/AlMahir/Data/Services/Local/SwiftDataService.swift
@@ -0,0 +1,93 @@
+//
+//  SwiftDataService.swift
+//  AlMahir
+//
+//  Created by Basmala Abuzied Ahmed on 17/07/2026.
+//
+
+import SwiftData
+import Foundation
+
+class SwiftDataService: SwiftDataServiceProtocol {
+    static let shared = SwiftDataService()
+    
+    private var container: ModelContainer?
+    private var context: ModelContext?
+    
+    private init() {}
+    
+    @MainActor func setup(schema: Schema) throws {
+        do {
+            self.container = try ModelContainer(for: schema)
+            self.context = self.container?.mainContext
+        } catch {
+            throw SwiftDataError.initializationFailed(error)
+        }
+    }
+    
+    
+    func fetchAll<T: PersistentModel>() throws -> [T] {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        let descriptor = FetchDescriptor<T>()
+        return try context.fetch(descriptor)
+    }
+    
+    func insert<T: PersistentModel>(_ item: T) throws {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        context.insert(item)
+        try save()
+    }
+    
+    func delete<T: PersistentModel>(_ item: T) throws {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        context.delete(item)
+        try save()
+    }
+    
+    func save() throws {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+}
+
+extension SwiftDataService {
+    
+    func fetchAll<T: PersistentModel>(sortBy: SortDescriptor<T>...) throws -> [T] {
+        var descriptor = FetchDescriptor<T>()
+        descriptor.sortBy = sortBy
+        return try fetchAll(with: descriptor)
+    }
+    
+    func fetchAll<T: PersistentModel>(with descriptor: FetchDescriptor<T>) throws -> [T] {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        return try context.fetch(descriptor)
+    }
+    
+    func fetch<T: PersistentModel>(predicate: Predicate<T>?) throws -> [T] {
+        guard let context = context else {
+            throw SwiftDataError.containerNotInitialized
+        }
+        var descriptor = FetchDescriptor<T>()
+        descriptor.predicate = predicate
+        return try context.fetch(descriptor)
+    }
+    
+    func deleteAll<T: PersistentModel>(_ type: T.Type) throws {
+        let items: [T] = try fetchAll()
+        for item in items {
+            try delete(item)
+        }
+    }
+}
