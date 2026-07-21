26de91853c184a1f2fb4701b503cc41c81e89849
alaaayman77 <alaaaymanfouad@gmail.com>
2026-07-21 18:11:47 +0300

    added domain entities and home repository protocol

---
+++ Modules/Home/Sources/Home/Domain/Entities/AyahOfTheDayEntity.swift
@@ -0,0 +1,15 @@
+//
+//  AyahOfTheDayEntity.swift
+//  Home
+//
+//  Created by Alaa Ayman on 07/02/1448 AH.
+//
+
+public struct AyahOfTheDayEntity {
+    public let arabicText: String
+    public let translation: String
+    public let surahName: String
+    public let ayahNumber: Int
+    public let juzNumber: Int
+    public let pageNumber: Int
+}
