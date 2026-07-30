b1d7e8148c3046aa0c92a606995869c3eac2f1a5
Esraa Ehab <esraaehab530@gmail.com>
2026-07-30 11:54:14 -0700

    added mualem di assembly

---
+++ Modules/Mualem/Sources/Mualem/DI/MuallimAssembly.swift
@@ -0,0 +1,35 @@
+//
+//  MuallimAssembly.swift
+//  Mualem
+//
+
+import Foundation
+import Swinject
+
+public final class MuallimAssembly: Assembly {
+    
+    public init() {}
+    
+    public func assemble(container: Container) {
+        
+        // Data Layer (Mock AI)
+        container.register(VoiceEvaluationServiceProtocol.self) { _ in
+            MockVoiceEvaluationService()
+        }.inObjectScope(.container)
+        
+        // Domain Layer (Use Cases)
+        container.register(EvaluateRecitationUseCase.self) { r in
+            EvaluateRecitationUseCase(evaluator: r.resolve(VoiceEvaluationServiceProtocol.self)!)
+        }
+        
+        // Presentation Layer (ViewModel)
+        container.register(MuallimViewModel.self) { r in
+            MainActor.assumeIsolated {
+                MuallimViewModel(
+                    audioService: r.resolve(AudioPlaybackServiceProtocol.self)!,
+                    evaluateUseCase: r.resolve(EvaluateRecitationUseCase.self)!
+                )
+            }
+        }
+    }
+}
