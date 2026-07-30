5d27ca5bda52b129bee7edb011adc283baad91fa
Esraa Ehab <esraaehab530@gmail.com>
2026-07-30 11:51:14 -0700

    edit malem package file

---
+++ Modules/Mualem/Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mualem",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Mualem",
+            targets: ["Mualem"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Swinject/Swinject.git",
+            exact: "2.9.1"
+        ),
+        .package(path: "../Common"),
+        .package(path: "../Mushaf")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Mualem",
+            dependencies: [
+                .product(name: "Swinject", package: "Swinject"),
+                .product(name: "Common", package: "Common"),
+                .product(name: "Mushaf", package: "Mushaf")
+            ]
+        ),
+        .testTarget(
+            name: "MualemTests",
+            dependencies: ["Mualem"]),
+    ]
+)
