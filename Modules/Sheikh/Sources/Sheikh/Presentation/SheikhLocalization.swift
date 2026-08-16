34923a905f572495977471c3e049f36c2cc16045
Nadin Ahmed <nadinahmed316@gmail.com>
2026-08-17 02:03:31 +0300

    Unify Sheikh localization usage across views

--- Modules/Sheikh/Sources/Sheikh/Presentation/SheikhLocalization.swift
+++ Modules/Sheikh/Sources/Sheikh/Presentation/SheikhLocalization.swift
@@ -5,3 +5,10 @@ import Foundation
 internal func localizedSheikhString(_ key: String) -> String {
     LanguageManager.localizedString(key, bundle: .module)
 }
+
+public extension SheikhAvailabilityStatus {
+    @MainActor
+    var localizedTitle: String {
+        localizedSheikhString(displayTitle)
+    }
+}
